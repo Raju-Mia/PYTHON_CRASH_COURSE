c52f7aea8bad81bba0e6434d8bf2b524b5dbe4c9
Raju-Mia <rajucse00@gmail.com>
2022-10-02 23:05:57 +0600

    This is the first project of PYTHON CRUSH COURSE book.

---
+++ getting_starting_with_django/learning_logs/views.py
@@ -0,0 +1,24 @@
+from multiprocessing import context
+from django.shortcuts import render
+import learning_logs
+
+from learning_logs.models import Topic
+
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def topics(request):
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'topics.html', context)
+
+
+def topic(request, topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries':entries}
+    return render(request, 'topic.html',context)
