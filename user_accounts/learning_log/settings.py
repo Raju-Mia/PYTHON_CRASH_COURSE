b65aff0a291d01d80ae4a240e29c37dd03148b37
Raju-Mia <rajucse00@gmail.com>
2022-11-12 01:14:16 +0600

    work on users app for logout-just creata default logout page and derirect by seeting.py

--- user_accounts/learning_log/settings.py
+++ user_accounts/learning_log/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,8 +44,9 @@ INSTALLED_APPS = [
 
     #for default django user accounts
     'users',
-
     
+
+
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#you can sett logout redirect url
+LOGOUT_REDIRECT_URL = 'users:login'
--- user_accounts/users/urls.py
+++ user_accounts/users/urls.py
@@ -1,6 +1,7 @@
 """defines url PATTERNS FOR U users"""
 
 from django.urls import path, include
+from .import views
 
 app_name = 'users'
 urlpatterns = [
